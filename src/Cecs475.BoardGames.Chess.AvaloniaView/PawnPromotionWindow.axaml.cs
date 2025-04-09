1331467011d429024dc52ab99f7e6f6d771e5561
lilistasos <anastasios.lilis@gmail.com>
2025-04-09 13:41:25 -0700

    Added Pawn Promotion Files

---
+++ src/Cecs475.BoardGames.Chess.AvaloniaView/PawnPromotionWindow.axaml.cs
@@ -0,0 +1,25 @@
+using Cecs475.BoardGames;
+using Cecs475.BoardGames.AvaloniaView;
+using Cecs475.BoardGames.Chess.Model;
+using Cecs475.BoardGames.Model;
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+
+namespace Cecs475.BoardGames.Chess.AvaloniaView
+{
+
+    public partial class PawnPromotionWindow : Window
+    {
+        public PawnPromotionWindow()
+        {
+            //InitializeComponent();
+        }
+    }
+}
